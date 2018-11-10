55fae4a90f9429275825d5557afbeabad5980686
study_sun <s6530085@hotmail.com>
2018-11-11 00:20:44 +0800

    Initial Commit

---
+++ GeekTimeAlgorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GeekTimeAlgorithm
+//
+//  Created by study_sun on 2018/11/11.
+//  Copyright © 2018 study_sun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
